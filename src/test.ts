975d0b68f57820427e6fc6189339dbce311ae0b3
Venu Nathan <venunathan12@gmail.com>
2025-10-20 16:57:41 +0530

    added template for examples

--- src/test.ts
+++ src/test.ts
@@ -10,10 +10,19 @@ if (tid === undefined)
 {
     let timestart = performance.now();
 
+    count = 0; successes = 0; failures = 0;
     let tests = (await fs.readdir('./tests')).filter(x => /^[0-9]+\_[0-9a-zA-Z]+$/.test(x)).sort();
     for (let test of tests) await Test(`./tests/${test}`);
+    let [testcount, testpass, testfail] = [count, successes, failures];
 
-    console.log('\n\n'); console.log(`${successes}/${count} Testcases Passed.\n${failures}/${count} Testcases Failed.\n`);
+    count = 0; successes = 0; failures = 0;
+    let examples = (await fs.readdir('./examples')).filter(x => /^[0-9]+\_[0-9a-zA-Z]+$/.test(x)).sort();
+    for (let example of examples) await Test(`./examples/${example}`);
+    let [excount, expass, exfail] = [count, successes, failures];
+
+    console.log('\n\n');
+    console.log(`${testpass}/${testcount} Testcases Passed.\n${testfail}/${testcount} Testcases Failed.\n`);
+    console.log(`${expass}/${excount} Examples Passed.\n${exfail}/${excount} Examples Failed.\n\n`);
 
     let timeend = performance.now();
     console.log(`Completed in ${ timeend - timestart } ms.`);
@@ -52,7 +61,7 @@ async function Test(tid: string)
             actualresult = detector.Assert(datajson, expectedtype, null, true) ? 'T' : 'F';
         } catch (e) {}
         let teststatus = actualresult === expectedresult ? 'PASS' : 'FAIL';
-        if (actualresult == null) teststatus += ' ' + '(RE)';
+        if (actualresult == null) teststatus += ' ' + '(Runtime Error)';
         count ++; if (teststatus == 'PASS') successes ++; else failures ++;
 
         console.log(teststatus);
